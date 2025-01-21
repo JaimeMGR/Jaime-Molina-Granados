4b0afe31254d2f5e63d51a6057510fb2e6bbae94
JaimeMGR <123658735+JaimeMGR@users.noreply.github.com>
2025-01-21 13:51:26 +0100

    Primer commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+ // Fade-in effect on scroll
+ const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+});
+
+document.querySelectorAll('.fade-in').forEach(section => {
+    observer.observe(section);
+});

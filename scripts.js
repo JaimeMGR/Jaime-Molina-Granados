4b93dcef15ffcec91c73ab3dcaee73d568fa52b0
JaimeMGR <123658735+JaimeMGR@users.noreply.github.com>
2025-02-10 09:20:52 +0100

    read more section

--- scripts.js
+++ scripts.js
@@ -27,3 +27,17 @@ window.addEventListener('load', (event) => {
         menuLateral.classList.toggle("visible");
     }
 });
+
+$(document).ready(function() {
+    $(".more").on("click", function() {
+      // cambiar la visibilidad de complete
+      $(".complete").toggle();
+  
+      // cambiar el texto del boton dependiendo del texto actual
+      if ($(this).text() == "Leer menos...") {
+        $(this).text("Leer mas...");
+      } else {
+        $(this).text("Leer menos...");
+      }
+    });
+  });
